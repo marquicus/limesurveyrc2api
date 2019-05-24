8c89aa41fc0b815f58840a746a5a6838c2377fa0
plascual <pascal.thivierge@gmail.com>
2019-05-24 19:32:27 +0000

    Corrected code alignment

--- limesurveyrc2api/_survey.py
+++ limesurveyrc2api/_survey.py
@@ -4,8 +4,8 @@ from limesurveyrc2api.exceptions import LimeSurveyError
 from os.path import splitext
 from base64 import b64encode
 
-class _Survey(object):
 
+class _Survey(object):
     def __init__(self, api):
         self.api = api
 
@@ -18,19 +18,15 @@ class _Survey(object):
         :type username: String
         """
         method = "list_surveys"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", username or self.api.username)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("iSurveyID", username or self.api.username)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
         if response_type is dict and "status" in response:
             status = response["status"]
             error_messages = [
-                "Invalid user",
-                "No surveys found",
-                "Invalid session key"
+                "Invalid user", "No surveys found", "Invalid session key"
             ]
             for message in error_messages:
                 if status == message:
@@ -39,8 +35,7 @@ class _Survey(object):
             assert response_type is list
         return response
 
-    def list_questions(self, survey_id,
-                       group_id=None, language=None):
+    def list_questions(self, survey_id, group_id=None, language=None):
         """
         Return a list of questions from the specified survey.
 
@@ -53,24 +48,18 @@ class _Survey(object):
         :type language: String
         """
         method = "list_questions"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", survey_id),
-            ("iGroupID", group_id),
-            ("sLanguage", language)
-        ])
+        params = OrderedDict([("sSessionKey",
+                               self.api.session_key), ("iSurveyID", survey_id),
+                              ("iGroupID", group_id), ("sLanguage", language)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
         if response_type is dict and "status" in response:
             status = response["status"]
             error_messages = [
-                "Error: Invalid survey ID",
-                "Error: Invalid language",
+                "Error: Invalid survey ID", "Error: Invalid language",
                 "Error: IMissmatch in surveyid and groupid",
-                "No questions found",
-                "No permission",
-                "Invalid session key"
+                "No questions found", "No permission", "Invalid session key"
             ]
             for message in error_messages:
                 if status == message:
@@ -98,27 +87,27 @@ class _Survey(object):
         
         """
         method = "add_survey"
-        params = OrderedDict([("sSessionKey", self.api.session_key),
-                              ("iSurveyID", desired_survey_id),
-                              ("sSurveyTitle ", survey_title),
-                              ("sSurveyLanguage  ", default_survey_language),
-                              ("sformat  ", appearance_format)])
+        params = OrderedDict(
+            [("sSessionKey", self.api.session_key),
+             ("iSurveyID", desired_survey_id), ("sSurveyTitle ", survey_title),
+             ("sSurveyLanguage  ",
+              default_survey_language), ("sformat  ", appearance_format)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
         if response_type is dict and "status" in response:
             status = response["status"]
-            error_messages = ["No permission", 
-                              "Invalid session key", 
-                              "Faulty parameters",
-                              "Creation Failed result"]
+            error_messages = [
+                "No permission", "Invalid session key", "Faulty parameters",
+                "Creation Failed result"
+            ]
             for message in error_messages:
                 if status == message:
                     raise LimeSurveyError(method, status)
         else:
             assert response_type is int
         return response
-    
+
     def delete_survey(self, survey_id):
         """ Delete a survey.
         
@@ -127,19 +116,14 @@ class _Survey(object):
         :type: Integer
         """
         method = "delete_survey"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", survey_id)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("iSurveyID", survey_id)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
         if response_type is dict and "status" in response:
             status = response["status"]
-            error_messages = [
-                "No permission",
-                "Invalid session key"
-            ]
+            error_messages = ["No permission", "Invalid session key"]
             for message in error_messages:
                 if status == message:
                     raise LimeSurveyError(method, status)
@@ -147,10 +131,16 @@ class _Survey(object):
             assert response_type is list
         return response
 
-    def export_responses(self, survey_id, document_type, language_code=None,
-                         completion_status='all', heading_type='code',
-                         response_type='short', from_response_id=None,
-                         to_response_id=None, fields=None):
+    def export_responses(self,
+                         survey_id,
+                         document_type,
+                         language_code=None,
+                         completion_status='all',
+                         heading_type='code',
+                         response_type='short',
+                         from_response_id=None,
+                         to_response_id=None,
+                         fields=None):
         """ Export responses in base64 encoded string.
         
         Parameters
@@ -175,18 +165,16 @@ class _Survey(object):
         :type fields: Array
         """
         method = "export_responses"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", survey_id),
-            ("sDocumentType", document_type),
-            ("sLanguageCode", language_code),
-            ("sCompletionStatus", completion_status),
-            ("sHeadingType", heading_type),
-            ("sResponseType", response_type),
-            ("iFromResponseID", from_response_id),
-            ("iToResponseID", to_response_id),
-            ("aFields", fields)
-        ])
+        params = OrderedDict(
+            [("sSessionKey", self.api.session_key), ("iSurveyID", survey_id),
+             ("sDocumentType",
+              document_type), ("sLanguageCode",
+                               language_code), ("sCompletionStatus",
+                                                completion_status),
+             ("sHeadingType", heading_type), ("sResponseType", response_type),
+             ("iFromResponseID",
+              from_response_id), ("iToResponseID", to_response_id), ("aFields",
+                                                                     fields)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
@@ -195,8 +183,7 @@ class _Survey(object):
             error_messages = [
                 "Language code not found for this survey.",
                 "No Data, could not get max id.",
-                "No Data, survey table does not exist",
-                "No permission",
+                "No Data, survey table does not exist", "No permission",
                 "Invalid session key"
             ]
             for message in error_messages:
@@ -206,7 +193,9 @@ class _Survey(object):
             assert response_type is str
         return response
 
-    def import_survey(self, path_to_import_survey, new_name=None,
+    def import_survey(self,
+                      path_to_import_survey,
+                      new_name=None,
                       dest_survey_id=None):
         """ Import a survey. Allowed formats: lss, csv, txt or lsa
 
@@ -234,13 +223,11 @@ class _Survey(object):
             import_data = import_data.decode('ascii')
 
         method = "import_survey"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("sImportData", import_data),
-            ("sImportDataType", import_datatype),
-            ("sNewSurveyName", new_name),
-            ("DestSurveyID", dest_survey_id)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("sImportData", import_data), ("sImportDataType",
+                                                             import_datatype),
+                              ("sNewSurveyName", new_name), ("DestSurveyID",
+                                                             dest_survey_id)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
@@ -267,10 +254,8 @@ class _Survey(object):
         :type survey_id: Integer
         """
         method = "activate_survey"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", survey_id)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("iSurveyID", survey_id)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
@@ -301,11 +286,9 @@ class _Survey(object):
         :type attribute_fields: Array
         """
         method = "activate_tokens"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyId", survey_id),
-            ("aAttributeFields", attribute_fields)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("iSurveyId", survey_id), ("aAttributeFields",
+                                                         attribute_fields)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
@@ -314,8 +297,7 @@ class _Survey(object):
             error_messages = [
                 "Error: Invalid survey ID",
                 "Survey participants table could not be created",
-                "No permission",
-                "Invalid session key"
+                "No permission", "Invalid session key"
             ]
             for message in error_messages:
                 if status == message:
@@ -332,10 +314,8 @@ class _Survey(object):
         :rtype survey_id: Integer
         """
         method = "list_groups"
-        params = OrderedDict([
-            ("sSessionKey", self.api.session_key),
-            ("iSurveyID", survey_id)
-        ])
+        params = OrderedDict([("sSessionKey", self.api.session_key),
+                              ("iSurveyID", survey_id)])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
 
