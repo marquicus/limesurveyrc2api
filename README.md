9875c02bceb877f70ae303139e1e2fe77d1acb19
Marco A Rojas <marquicus@gmail.com>
2020-02-26 09:24:33 -0600

    [ENH] set_participant_properties

--- README.md
+++ README.md
@@ -55,6 +55,7 @@ It's just a start, so the list of implemented methods is shorter than not.
   + add_participants
   + delete_participants
   + get_participant_properties
+  + set_participant_properties
   + get_summary
   + invite_participants
   + list_participants
--- limesurveyrc2api/_token.py
+++ limesurveyrc2api/_token.py
@@ -280,9 +280,9 @@ class _Token(object):
     def remind_participants(self, survey_id, min_days_between=None,
                             max_reminders=None, token_ids=False):
         """ Send a reminder to participants in a survey.
-        
+
         Returns result of sending.
-        
+
         Parameters
         :param survey_id: ID of the Survey that participants belong.
         :type survey_id: Integer
@@ -318,4 +318,65 @@ class _Token(object):
                     raise LimeSurveyError(method, status)
         else:
             assert response_type is list
+        return response
+
+    def set_participant_properties(
+            self, survey_id, token_id, token_query_properties=None,
+            token_data={}):
+        """
+        Set participant properties (by token) from the specified survey.
+
+        Provide either token_id or token_query_dict, not both.
+
+        For token properties for querying and return, choose from:
+
+        ["blacklisted", "completed", "email", "emailstatus", "firstname",
+         "language", "lastname", "mpid", "participant_id", "remindercount",
+         "remindersent", "sent", "tid", "token", "usesleft", "validfrom",
+         "validuntil"]
+
+        Parameters
+        :param survey_id: ID of survey to get participant properties for.
+        :type survey_id: Integer
+        :param token_id: ID of participant to get properties for.
+        :type token_id: Integer
+        :param token_query_properties: Key(s) / value(s) to use for finding the
+          participant among all those that are in the survey.
+        :type token_query_properties: Dict[String, Any]
+        :param token_properties: Keys to return from RPC call.
+        :type token_properties: List[String]
+        """
+        method = "set_participant_properties"
+        if token_id is not None and token_query_properties is not None:
+            raise ValueError(
+                "Provide either token_id or token_query_dict, not both.")
+        if token_query_properties is None:
+            token_query_properties = {"tid": token_id}
+
+        params = OrderedDict([
+            ("sSessionKey", self.api.session_key),
+            ("iSurveyID", survey_id),
+            ("aTokenQueryProperties", token_query_properties),
+            ("aTokenData", token_data)
+        ])
+        response = self.api.query(method=method, params=params)
+        response_type = type(response)
+
+        if response_type is dict and "status" in response:
+            status = response["status"]
+            error_messages = [
+                "Error: Invalid survey ID",
+                "Error: No token table",
+                "Error: No results were found based on your attributes.",
+                "Error: More than 1 result was found based on your attributes.",
+                "Error: Invalid tokenid",
+                "No valid Data",
+                "No permission",
+                "Invalid Session Key"
+            ]
+            for message in error_messages:
+                if status == message:
+                    raise LimeSurveyError(method, status)
+        else:
+            assert response_type is dict
         return response
